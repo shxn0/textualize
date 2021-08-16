632b23a2d9442fb9d204b71a3763b15ffb16ebec
Shinnosuke Yoshizawa <yoshizawashinnosuke@LAB-N1516.local>
2021-08-17 00:26:32 +0900

    add chars search function

--- src/app.py
+++ src/app.py
@@ -5,16 +5,13 @@ import streamlit as st
 import pandas as pd
 from word import Word
 from janome.tokenizer import Tokenizer
-
 from language import Language
-
 
 def main():
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'config/secret.json'
 
     # ファイルを読み込んでテキスト化する
     def transcribe_file(content, lang = Language.ENG.value[1]) -> str:
-
         client = speech.SpeechClient()
         audio = speech.RecognitionAudio(content = content)
         config = speech.RecognitionConfig(
@@ -68,8 +65,31 @@ def main():
                 lumps.append(word_list[:word.index+6])
             else: lumps.append(word_list[word.index-5:word.index+6])
         return lumps
+
+
+    # 入力された文字列の前後5文字を抽出
+    def extract_chars(input_words, text, lang) -> list[tuple]:
+        input_words = [st for st in input_words if st != '']
+        if lang == Language.ENG.value[0]:
+            text = ''.join(text.split(' '))
+
+        for word in input_words:
+            list = []
+            target = word
+            n = 0
+            for i in range(0, len(text)):      
+                index = text.find(target, n)
+                n = index + len(target)
+                if index == -1:
+                    break
+                if index < 5:
+                    list.append((index, target, text[:index+len(target)+5]))
+                else: 
+                    list.append((index, target, text[index-5:index+len(target)+5]))
+        return list
+
     
-    
+    # 画面表示用のテーブルを作成する
     def create_table(lumps):
         df = pd.DataFrame(data = map(lambda l: map(lambda w: w.name, l), lumps))
         df.index = df.index + 1
@@ -90,7 +110,7 @@ def main():
         t = Tokenizer()
         return t.tokenize(text, wakati=True)
 
-
+    
     # Streamlitへの書き出し
     st.title('Transcription App')
     st.header('Overview')
@@ -133,11 +153,12 @@ def main():
         option = st.selectbox('Select a language for translation', (Language.ENG.value[0], Language.JP.value[0]))
         st.session_state.lang = option
 
-        st.write('Transcription')
+        st.subheader('Transcription')
         if st.button('Start'):
             comment = st.empty()
             comment.write('Analyzing..')
             st.session_state.result = transcribe_file(content, lang = Language.get_code(option))
+            # 文字起こし後のダミーデータ
             # st.session_state.result = "Erin how can I help you today I see you signed up for a course online and I just cannot access that I've been trying the last 4 hours download display doesn't do anything"
             # st.session_state.result = 'ご住所の変更でございますねご連絡ありがとうございます恐れ入りますがご契約内容を確認いたしますのでお電話を頂いてる方は契約者ご本人様でいらっしゃいますかはいそうです本人ですそれではお電話をいただいておりますお客様のお名前をお願い致します山田太郎です'
             comment.write('')
@@ -145,7 +166,6 @@ def main():
             st.write(st.session_state.result)
 
         if st.session_state.result:
-            # 複数検索フォーム入力
             st.title('Search Words')
             input_words = []
             with st.form(key = 'search'):
@@ -157,20 +177,30 @@ def main():
                     input_words.append(word1)
                 with col2:
                     word2 = st.text_input(label = '2', key = 'word_2')
-                    input_words.append(word2)        
+                    input_words.append(word2)
                 with col3:
                     word3 = st.text_input(label = '3', key = 'word_3')
-                    input_words.append(word3)        
+                    input_words.append(word3)
                     st.form_submit_button(label = "Search")
 
             matched_words = search(input_words)
             lumps = extract_words(matched_words)
+
+            text = st.session_state.result
+            lang = st.session_state.lang
+
+            if input_words[0] is not '' or input_words[1] is not '' or input_words[2] is not '':
+                st.subheader('Results')
+                st.subheader('Char Classification')
+                chars = extract_chars(input_words, text, lang)
+                for char in chars:
+                    st.write(f'Position={char[0]+1}, Word={char[1]}, Chars={char[2]}')
             
             if len(lumps) is not 0:
-                st.subheader('Results')
+                st.subheader('Word Classification')
                 for w in matched_words:
                     st.write(f'Position={w.index + 1}, Word={w.name}')
-                st.subheader('Words around the search word')
+                st.subheader('5 Words before/after the search word')
                 create_table(lumps)
 
                     
